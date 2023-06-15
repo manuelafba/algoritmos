374309be6e5e291cc68a8d0679ad8a8b3ccb5d8f
manuela araújo <104527846+manuelafba@users.noreply.github.com>
2023-06-15 10:35:31 -0300

    Add files via upload

--- aula 09 matrizes/exercicio04.py
+++ aula 09 matrizes/exercicio04.py
@@ -1,14 +1,14 @@
 matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 resultado = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
-for c in range(3):  # coluna
-    for l in range(3):  # linha
+for l in range(3):  # linha
+    for c in range(3):  # coluna
         matriz[l][c] = int(input(f'Digite o {l + 1}º elemento da {c + 1}ª coluna da matriz: '))
 
-for i in range(len(matriz)): #linhas
-    for j in range(len(matriz)): #colunas
+for l in range(len(matriz)): #linhas
+    for c in range(len(matriz)): #colunas
         for k in range(len(matriz)): #colunas e linhas
-            resultado[i][j] += matriz[i][k] * matriz[k][j]
+            resultado[l][c] += matriz[l][k] * matriz[k][c]
 
 for l in range(3):  
     for c in range(3):  
